8127eed04043095d3bf57a474f08ef542349954b
priest davos <rajjo1920london@gmail.com>
2024-03-16 19:45:29 +0530

    created a useState for time and use useEffect Hook in useEffectHook  useEffect(() => {     const intervalId = setInterval(() => { setCurrentTime(new Date()) }, 1000);     return () => {       clearInterval(intervalId);  // Cleanup function to stop the interval when the component unmounts       // console.log("Cancelled the interval")     }   }, []   )

--- src/components/showTime.jsx
+++ src/components/showTime.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react"
+
 function ShowTime() {
-  // let current_time = Time.current_time
-  let time = new Date();
+
+  // create a useState for time
+  const [currentTime, setCurrentTime] = useState(new Date())
+
+  // create a useEfect
+  useEffect(() => {
+
+    // console.log("Interval has been setup")
+   
+    const intervalId = setInterval(() => { setCurrentTime(new Date()) }, 1000); //setInterval take two arg, first-> method, second->time
+
+    return () => {
+      clearInterval(intervalId);  // Cleanup function to stop the interval when the component unmounts
+      // console.log("Cancelled the interval")
+    }
+  }, []
+  )
 
 
   return (
-    <p >--------------{time.toLocaleDateString()} - {time.toLocaleTimeString()}----------------</p>
+    <p >--------------{currentTime.toLocaleDateString()} - {currentTime.toLocaleTimeString()}----------------</p>
   )
 }
 export default ShowTime
